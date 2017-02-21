4cf91f983e1abe29b2d6114ccc5f881264fa83c9
LEEHOONY <gnswodl123@gmail.com>
2017-02-21 23:49:28 +0900

    Add week-02-django project
    started blog app

---
+++ week02_django/django_sample/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hun\hoony\cording-for-office-workers\week02_django\django_sample\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
