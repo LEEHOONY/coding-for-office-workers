91463a8038b4e9b73a5e48ca9d23cfea01501adf
LEEHOONY <gnswodl123@gmail.com>
2017-02-25 23:56:45 +0900

    django

--- week02_django/src/blog/admin.py
+++ week02_django/src/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article, comment
 
 # Register your models here.
+@admin.register(Article, comment)
+class BlogAdmin(admin.ModelAdmin):
+    pass
